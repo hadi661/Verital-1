de8f8673b4ce7fc255f362773a8960e8ab88a0fe
hadi djemel <hadidjemel2@gmail.com>
2024-07-09 14:17:31 +0100

    update 09/07/2024

--- server/models/about.js
+++ server/models/about.js
@@ -1,29 +1,28 @@
-// models/about.js
 const mongoose = require('mongoose');
-const { Schema } = mongoose;
 
-const  MultilingualContentSchema= new Schema({
-    en: { type: String, required: true },
-    fr: { type: String, required: true },
-    ar: { type: String, required: true },
-  }, { _id: false });
-  
-  const QuoteSchema = new Schema({
-    en: { type: String, required: false },
-    fr: { type: String, required: false },
-    ar: { type: String, required: false },
-  }, { _id: false });
-  
 const aboutSchema = new mongoose.Schema({
-    title: { type: MultilingualContentSchema, required: true },
-    content: { type: MultilingualContentSchema, required: true },
-    quotes: { type: [QuoteSchema], required: false },
-    text: { type: MultilingualContentSchema, required: true },
-    additionalContent: { type: MultilingualContentSchema, default: null },
-    images: { type: [String], required: false },
-    videos: { type: [String], required: false },
+  title: {
+    en: String,
+    fr: String,
+    ar: String
+  },
+  content: {
+    en: String,
+    fr: String,
+    ar: String
+  },
+  text: {
+    en: String,
+    fr: String,
+    ar: String
+  },
+  additionalContent: {
+    en: [String],
+    fr: [String],
+    ar: [String]
+  },
+  images: [String],
+  videos: [String]
 });
 
-const About = mongoose.model('About', aboutSchema);
-
-module.exports = About;
+module.exports = mongoose.model('About', aboutSchema);
--- server/routes/main.js
+++ server/routes/main.js
@@ -341,6 +341,7 @@ router.get('/about', async (req, res) => {
             title: "Verital Spa",
             description: "siège veritas alger direction general DG",
         };
+        
         const aboutData = await About.findOne({}, {
             [`title.${lang}`]: 1,
             [`content.${lang}`]: 1,
