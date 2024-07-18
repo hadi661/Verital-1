c03ae6293338976be89abb8fa5a8c9787ad122c5
hadi djemel <hadidjemel2@gmail.com>
2024-07-18 15:08:28 +0100

    Removed specified files

--- middleware/auth.js
+++
@@ -1,9 +0,0 @@
-// middleware/auth.js
-function ensureAuthenticated(req, res, next) {
-    if (req.session.user && req.session.user.role === 'admin') {
-        return next();
-    }
-    res.redirect('/login');
-}
-
-module.exports = ensureAuthenticated;
